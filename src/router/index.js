c890ff919c47d5453bfc94fd7d2a7d893e373d5c
Houndini <sanonloick0@gmail.com>
2024-05-03 18:47:17 -0400

    Lazy Loading routes...

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import SessionView from '../views/SessionView.vue'
-import InboxView from '../views/InboxView.vue'
-import SendMessageView from '../views/SendMessageView.vue'
+// import HomeView from '../views/HomeView.vue'
+// import SessionView from '../views/SessionView.vue'
+// import InboxView from '../views/InboxView.vue'
+// import SendMessageView from '../views/SendMessageView.vue'
 
-import RoomView from '../views/RoomView.vue'
+const InboxView = () => import('../views/InboxView.vue');
+const SendMessageView = () => import('../views/SendMessageView.vue');
+const SessionView = () => import('../views/SessionView.vue');
+const HomeView = () => import('../views/HomeView.vue');
+const RoomView = () => import('../views/RoomView.vue');
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
