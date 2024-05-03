1c320cf36550e67275992730323e507edeb2a1a3
Houndini <sanonloick0@gmail.com>
2024-05-02 21:56:14 -0400

    Some styles.........

--- src/views/SendMessageView.vue
+++ src/views/SendMessageView.vue
@@ -15,7 +15,7 @@
         <span> {{ feedback }}</span>
     </div>
     <form @submit.prevent="submitMssg">
-        <textarea :placeholder="'Send an anonymous message to' + ' ' + usernameRef" v-model="messageRef" ></textarea>
+        <textarea placeholder="Type your anonymous message" v-model="messageRef" ></textarea>
         <button type="submit">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M224.47,31.52a11.87,11.87,0,0,0-11.82-3L20.74,86.67a12,12,0,0,0-1.91,22.38L105,151l41.92,86.15A11.88,11.88,0,0,0,157.74,244c.34,0,.69,0,1,0a11.89,11.89,0,0,0,10.52-8.63l58.21-192,0-.08A11.85,11.85,0,0,0,224.47,31.52Zm-4.62,9.54-58.23,192a4,4,0,0,1-7.48.59l-41.3-84.86,50-50a4,4,0,1,0-5.66-5.66l-50,50-84.9-41.31a3.88,3.88,0,0,1-2.27-4,3.93,3.93,0,0,1,3-3.54L214.9,36.16A3.93,3.93,0,0,1,216,36a4,4,0,0,1,2.79,1.19A3.93,3.93,0,0,1,219.85,41.06Z"></path></svg>
         </button>
@@ -122,10 +122,11 @@ border-radius: 0.5em;
 
     margin: 10% auto 0;
     width: min(100%,40rem);
-    border: 1px solid  rgba(3,3,3,0.1);
+    background: rgb(108,174,182);
+    background: radial-gradient(circle, rgba(108,174,182,1) 0%, rgba(123,166,204,1) 0%, rgba(215,90,238,1) 100%, rgba(231,6,255,1) 100%);
     text-align: center;
     padding: 0.4em;
-    border-radius: 2em;
+    border-radius: 1em;
 }
 
 .container-header {
@@ -159,7 +160,8 @@ border-radius: 0.5em;
     resize: none;
     background-color: transparent;
     border-radius: 0.6em;
-    border: 1px solid  rgba(3,3,3,0.3);
+    border: none;
+    text-align: center;
     color: var(--vt-c-black);
 }
 
